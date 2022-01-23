21d85d079db51551b55dc8935a943d7199c423ec
Rodney Polkinghorne <604170+thisrod@users.noreply.github.com>
2022-01-23 15:39:35 +1100

    Select with single stream

--- cord.py
+++ cord.py
@@ -1,14 +1,31 @@
 """
 Acme interface for Rope
 
-Initial goal: print a message when a Python file is opened.
+Second goal: print every button-3 token in Python files.
+
+TODO Clean up when Python windows are closed
 """
 
-from subprocess import Popen, PIPE, DEVNULL, run
+# from subprocess import Popen, PIPE, DEVNULL
+from asyncio import run, wait, create_task, FIRST_COMPLETED
+from asyncio.subprocess import create_subprocess_exec, PIPE
 
-window_events = Popen(['9p', 'read', 'acme/log'], bufsize=0, text=True, stdout=PIPE, stderr=PIPE)
-print('Started', flush=True)
-while True:
-    wid, cmd, *name = window_events.stdout.readline().split()
-    if cmd == 'new' and name and name[0].endswith('.py'):
-       print(F'Opened {name[0]} in window {wid}', flush=True)
+async def line_reader(aStream):
+    return aStream, await aStream.readline()
+
+async def event_stream_for(s):
+    events = await create_subprocess_exec('9p', 'read', F'acme/{s}', stdout=PIPE, stderr=PIPE)
+    return events.stdout
+
+async def main():
+    window_stream = await event_stream_for('log')
+    print('Started', flush=True)
+    while True:
+        done, pending = await wait([line_reader(window_stream)], return_when=FIRST_COMPLETED)
+        x, txt = await next(t for t in done)
+        assert x == window_stream
+        wid, cmd, *name = txt.decode().split()
+        if cmd == 'new' and name and name[0].endswith('.py'):
+           print(F'Opened {name[0]} in window {wid}', flush=True)
+
+run(main())
