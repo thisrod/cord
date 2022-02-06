cea6912f03c3083595b5bde8b2164bda520afbe9
Rodney Polkinghorne <604170+thisrod@users.noreply.github.com>
2022-02-06 11:06:50 +1100

    Stub out how look for definition will work

--- cord.py
+++ cord.py
@@ -11,15 +11,17 @@ TODO Clean up when Python windows are closed
 from asyncio import run, create_task
 from asyncio.subprocess import create_subprocess_exec, PIPE
 
+
 async def log_stream():
-    return await nine_stream_for('acme/log')
+    return await nine_stream_for("acme/log")
+
 
 async def nine_stream_for(path):
-    process = await create_subprocess_exec('9p', 'read', path, stdout=PIPE, stderr=PIPE)
+    process = await create_subprocess_exec("9p", "read", path, stdout=PIPE, stderr=PIPE)
     return process.stdout
 
 
-class PythonWindow():
+class PythonWindow:
     def __init__(self, wid):
         self._wid = wid
 
@@ -29,18 +31,27 @@ class PythonWindow():
         return self._wid
 
     async def open_event_stream(self):
-        self.event_stream = await nine_stream_for(F'acme/{self.wid}/event')
+        self.event_stream = await nine_stream_for(f"acme/{self.wid}/event")
 
     async def handle_events(self):
         await self.open_event_stream()
         while True:
             event = await WindowEvent.scan(self.event_stream)
-            print(F'Event for window {self.wid}: {event}', flush=True)
+            print(f"Event for window {self.wid}: {event}", flush=True)
             # handle_event(txt.decode())
 
 
-class WindowEvent():
+class Editor:
+    """
+    An Acme instance with an associated rope project
 
+    Responsible for handling top-level Acme events
+    """
+
+    pass
+
+
+class WindowEvent:
     def __init__(self, origin, cause, start, end, flag, text):
         self.origin = origin
         self.cause = cause
@@ -51,9 +62,9 @@ class WindowEvent():
 
     def __repr__(self):
         return (
-            F'WindowEvent(origin={repr(self.origin)}, cause={repr(self.cause)}, '
-            F'start={repr(self.start)}, end={repr(self.end)}, '
-            F'flag={repr(self.flag)}, text={repr(self.text)})'
+            f"WindowEvent(origin={repr(self.origin)}, cause={repr(self.cause)}, "
+            f"start={repr(self.start)}, end={repr(self.end)}, "
+            f"flag={repr(self.flag)}, text={repr(self.text)})"
         )
 
     @classmethod
@@ -62,26 +73,27 @@ class WindowEvent():
         s = s.decode()
         origin = s[0]
         cause = s[1]
-        start, end, flag, length, text_line = s[2:].split(sep=' ', maxsplit=4)
+        start, end, flag, length, text_line = s[2:].split(sep=" ", maxsplit=4)
         length = int(length)
-        while len(text_line) < length+1:
+        while len(text_line) < length + 1:
             s = await aStream.readline()
             text_line += s.decode()
-        assert text_line[length:] == '\n'
+        assert text_line[length:] == "\n"
         return cls(origin, cause, int(start), int(end), int(flag), text_line[:length])
 
 
 async def main():
     event_tasks = set()
     window_stream = await log_stream()
-    print('Started', flush=True)
+    print("Started", flush=True)
     while True:
         txt = await window_stream.readline()
         id, cmd, *name = txt.decode().split()
         window = PythonWindow(id)
-        if cmd == 'new' and name and name[0].endswith('.py'):
+        if cmd == "new" and name and name[0].endswith(".py"):
             event_tasks |= {create_task(window.handle_events())}
-            print(F'Opened {name[0]} in window {window.wid}', flush=True)
+            print(f"Opened {name[0]} in window {window.wid}", flush=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run(main())
--- test_all.py
+++ test_all.py
@@ -1,11 +1,12 @@
-from unittest import TestCase
+from unittest import TestCase, skip
+from unittest.mock import patch
 from io import BytesIO
 import asyncio
 
-from cord import WindowEvent
+from cord import WindowEvent, PythonWindow, Editor
+
 
 class AsyncBytesIO(BytesIO):
-
     async def readline(self):
         return super().readline()
 
@@ -25,22 +26,60 @@ class Event(TestCase):
         self.assertEqual(event.text, text)
 
     def test_key_press(self):
-        self.assertEvent(b'KI0 1 0 1 f\n', 'K', 'I', 0, 1, 0, 'f')
+        self.assertEvent(b"KI0 1 0 1 f\n", "K", "I", 0, 1, 0, "f")
 
     def test_return_press(self):
-        self.assertEvent(b'KI0 1 0 1 \n\n', 'K', 'I', 0, 1, 0, '\n')
+        self.assertEvent(b"KI0 1 0 1 \n\n", "K", "I", 0, 1, 0, "\n")
 
     def test_multiline(self):
-        self.assertEvent(b'ML0 13 0 13 cat\ndog\nmouse\n', 'M', 'L', 0, 13, 0, 'cat\ndog\nmouse')
+        self.assertEvent(
+            b"ML0 13 0 13 cat\ndog\nmouse\n", "M", "L", 0, 13, 0, "cat\ndog\nmouse"
+        )
 
     def test_look(self):
-        self.assertEvent(b'ML1 1 2 0 \n', 'M', 'L', 1, 1, 2, '')
+        self.assertEvent(b"ML1 1 2 0 \n", "M", "L", 1, 1, 2, "")
 
     def test_look_expanded(self):
-        self.assertEvent(b'ML0 3 0 3 foo\n', 'M', 'L', 0, 3, 0, 'foo')
+        self.assertEvent(b"ML0 3 0 3 foo\n", "M", "L", 0, 3, 0, "foo")
 
     def test_execute(self):
-        self.assertEvent(b'Mx13 13 3 0 \n', 'M', 'x', 13, 13, 3, '')
+        self.assertEvent(b"Mx13 13 3 0 \n", "M", "x", 13, 13, 3, "")
 
     def test_execute_expanded(self):
-        self.assertEvent(b'Mx12 15 0 3 Del\n', 'M', 'x', 12, 15, 0, 'Del')
+        self.assertEvent(b"Mx12 15 0 3 Del\n", "M", "x", 12, 15, 0, "Del")
+
+
+class WindowSetup(TestCase):
+    """Creating a window executes the expected rope actions"""
+
+    @skip
+    def test_construct(self):
+        with patch("rope.base.project.Project") as Project, patch.object(
+            PythonWindow, "path"
+        ) as path:
+            window = PythonWindow(666)
+            Project.find_module.assert_called_once_with(path)
+            self.assertEqual(
+                window.rope_module,
+                Project.find_module.assert_called_once_with.return_value,
+            )
+
+
+class Look(TestCase):
+    """Look events call rope symbol lookup"""
+
+    @skip
+    def test_handle_look(self):
+        with patch(
+            "rope.contrib.findit.find_definition"
+        ) as find_definition, patch.object(
+            PythonWindow, "rope_module"
+        ) as module, patch.object(
+            Editor, "rope_project"
+        ) as project:
+            window = Editor().window(666)
+            text = Mock()
+            start = Mock()
+            event = WindowEvent("M", "L", start, None, None, text)
+            window.handle_event(event)
+            find_definition.assert_called_once_with(project, start, text)
